2173f243ac9447456c79644046ef4cda404074b8
Callum White <callumw1991@gmail.com>
2016-03-11 11:43:30 +1100

    Start using Simple Form

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,5 +4,19 @@ class Beer < ActiveRecord::Base
   validates :style, presence: true
   validates :abv, presence: true
 
-  
+  def name=(s)
+    super(s.titleize)
+  end
+
+  def brewer=(s)
+    super(s.titleize)
+  end
+
+  def style=(s)
+    super(s.capitalize)
+  end
+
+  def abv=(p)
+    super(p.to_f)
+  end
 end
